7f57205c658b575173ef9de2bb5eff96fa2d1ccd
Tami Zwick <tzwick@factsmgt.com>
2019-01-09 08:47:45 -0600

    Installs autoprefixer, sets gulp.watch

--- Lesson 2/gulpfile.js
+++ Lesson 2/gulpfile.js
@@ -2,15 +2,21 @@ const gulp = require("gulp");
 
 gulp.task("default", function() {
   // code for your default task goes here
-    console.log("Testing Gulp");
+    gulp.watch("sass/**/*.scss", ["styles"]);
 });
 
 const sass = require("gulp-sass");
+const autoprefixer = require("gulp-autoprefixer");
 
 gulp.task("styles", function() {
     gulp
         .src("sass/**/*.scss")
         .pipe(sass())
         .on("error", sass.logError)
+        .pipe(
+            autoprefixer({
+                browsers: ["last 2 versions"]
+            })
+        )
         .pipe(gulp.dest("./css"));
 });
