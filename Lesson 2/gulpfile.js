5fe030c499ab39cd17972bc03fb92072c37e8435
Tami Zwick <tzwick@factsmgt.com>
2019-01-10 15:26:54 -0600

    Concatenates js files, minifies css

--- Lesson 2/gulpfile.js
+++ Lesson 2/gulpfile.js
@@ -9,6 +9,20 @@ gulp.task("default", ["copy-html", "copy-images", "styles"], function() {
 
 const sass = require("gulp-sass");
 const autoprefixer = require("gulp-autoprefixer");
+const concat = require("gulp-concat");
+
+gulp.task("scripts", function() {
+    gulp.src("js/**/*.js")
+        .pipe(concat("all.js"))
+        .pipe(gulp.dest("dist/js"));
+});
+
+gulp.task("scripts-dist", function() {
+    gulp.src("js/**/*.js")
+        .pipe(concat("all.js"))
+        .pipe(gulp.dest("dist/js"));
+    
+});
 
 gulp.task('copy-html', function() {
     gulp.src("./index.html")
@@ -23,7 +37,7 @@ gulp.task("copy-images", function() {
 gulp.task("styles", function() {
     gulp
         .src("sass/**/*.scss")
-        .pipe(sass())
+        .pipe(sass({outputStyle: "compressed"}))
         .on("error", sass.logError)
         .pipe(
             autoprefixer({
